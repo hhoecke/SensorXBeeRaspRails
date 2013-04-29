407592167db97b6f8ddee8e3b178d56757757c34
Hendrik <hendrik.hoecke@gmail.com>
2013-04-29 16:01:14 +0200

    Initialising project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SensorNetwork::Application.config.secret_token = 'bece2cba96bd9c5c5a5e7ed86ece15d3ef572df2948b8c2ae3a9ab73c447d7858844c75cf8de75391db9227766c12e4347a29473e094d91533bcb9e8cc583a09'
